108fea65f6d7532bdbbdb084fc01ef0666004009
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-02-15 16:03:02 +0000

    Keyblade code refactor
    uses my XIII.create_random_tag() function instead of doing it manually

--- content/cards/12_keyblade.lua
+++ content/cards/12_keyblade.lua
@@ -20,35 +20,7 @@ SMODS.Joker {
     discovered = true,
     blueprint_compat = false,
 
-    config = {
-        extra = {
-            options = {
-                [1]  = 'tag_uncommon',
-                [2]  = 'tag_rare',
-                [3]  = 'tag_negative',
-                [4]  = 'tag_foil',
-                [5]  = 'tag_holo',
-                [6]  = 'tag_polychrome',
-                [7]  = 'tag_investment',
-                [8]  = 'tag_voucher',
-                [9]  = 'tag_boss',
-                [10] = 'tag_standard',
-                [11] = 'tag_charm',
-                [12] = 'tag_meteor',
-                [13] = 'tag_buffoon',
-                [14] = 'tag_handy',
-                [15] = 'tag_garbage',
-                [16] = 'tag_ethereal',
-                [17] = 'tag_coupon',
-                [18] = 'tag_double',
-                [19] = 'tag_juggle',
-                [20] = 'tag_d_six',
-                [21] = 'tag_top_up',
-                [22] = 'tag_skip',
-                [23] = 'tag_economy',
-            }
-        }
-    },
+    config = {},
 
     calculate = function(self, card, context)
         if context.first_hand_drawn and not context.blueprint then
@@ -57,19 +29,8 @@ SMODS.Joker {
         end
         if context.destroy_card and not context.blueprint then
             local keyblade_card = G.GAME.current_round.keyblade_rank.rank or { rank = 'Seven' }
-            if #context.full_hand == 1
-                and context.destroy_card == context.full_hand[1]
-                and context.full_hand[1].base.value == keyblade_card
-                and G.GAME.current_round.hands_played == 0 then
-                local tag_choice = pseudorandom_element(card.ability.extra.options, pseudoseed('keyblade'))
-                G.E_MANAGER:add_event(Event({
-                    func = (function()
-                        add_tag(Tag(tag_choice))
-                        play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
-                        play_sound('holo1', 1.2 + math.random() * 0.1, 0.4)
-                        return true
-                    end)
-                }))
+            if #context.full_hand == 1 and context.destroy_card == context.full_hand[1] and context.full_hand[1].base.value == keyblade_card and G.GAME.current_round.hands_played == 0 then
+                XIII.create_random_tag(card)
                 return { remove = true }
             end
         end
